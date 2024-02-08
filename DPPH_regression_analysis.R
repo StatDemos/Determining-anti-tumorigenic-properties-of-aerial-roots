49fce6173af4617d1a152c77a7989e342866a91d
laknaperera98 <laknashenaliperera@gmail.com>
2024-02-08 10:21:45 +0530

    Fitted regression line

--- DPPH_regression_analysis.R
+++ DPPH_regression_analysis.R
@@ -12,12 +12,24 @@ DPPH %>%
 
 # Regression model
 # 1st order
-DPPH_regression_model1 <- lm(DPPH$`scavenging activity` ~ DPPH$concentration)
+DPPH_regression_model1 <- lm(`scavenging activity` ~ concentration, data = DPPH)
 summary(DPPH_regression_model1)
 
 
 # 2nd order
 DPPH$concentration2 <- DPPH$concentration*DPPH$concentration
 
-DPPH_regression_model2 <- lm(DPPH$`scavenging activity` ~ DPPH$concentration + DPPH$concentration2)
+DPPH_regression_model2 <- lm(`scavenging activity` ~ concentration + concentration2, data = DPPH)
 summary(DPPH_regression_model2)
+
+# Fitted regression line
+convalues <- seq(0, 400, 10)
+convalues2 <- convalues^2
+predictedcounts <- predict(DPPH_regression_model2, list(concentration=convalues, concentration2 = convalues2))
+
+plot(DPPH$concentration, DPPH$`scavenging activity`,
+     pch=16, 
+     xlab = "concentration", ylab = "scavenging activity", cex.lab = 1.3, 
+     col = "blue")
+
+lines(convalues, predictedcounts, col = "darkgreen", lwd = 3)
