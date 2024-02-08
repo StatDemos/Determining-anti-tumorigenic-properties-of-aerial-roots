8c17dc058c396ff9361798db306a8a3b6ed77459
laknaperera98 <laknashenaliperera@gmail.com>
2024-02-08 10:07:01 +0530

    Fitted regression line

--- Anti-inflammatory_regression_analysis.R
+++ Anti-inflammatory_regression_analysis.R
@@ -12,5 +12,17 @@ Anti_inflammatory %>%
 
 # Regression model
 # 1st order
-Anti_inf_regression_model1 <- lm(Anti_inflammatory$inhibition ~ Anti_inflammatory$concentration)
+Anti_inf_regression_model1 <- lm(inhibition ~ concentration, data = Anti_inflammatory)
 summary(Anti_inf_regression_model1)
+
+# Fitted regression line
+convalues <- seq(0, 4500, 100)
+predictedcounts <- predict(Anti_inf_regression_model1, list(concentration=convalues))
+
+plot(Anti_inflammatory$concentration, Anti_inflammatory$inhibition,
+     pch=16, 
+     xlab = "concentration", ylab = "inhibition", cex.lab = 1.3, 
+     col = "blue")
+
+lines(convalues, predictedcounts, col = "darkgreen", lwd = 3)
+
